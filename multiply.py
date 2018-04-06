7d294d05764e83ce2c80e394d4444f4b4c05bd3d
Shalmalee Kapse <shals91@gmail.com>
2018-04-06 15:27:02 +0530

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -6,6 +6,6 @@ import numpy                   # Imports the numpy package
 A = numpy.loadtxt("data.dat")  # Reads the data in the file named data.dat
 #print A                       # Prints the data in data.dat stored in A 
 
-A[:,1] *= 2.0 				         # Multiplies the second column in A by any number (Here, I took 2)
+A[:,1] *= 2.0 				# Multiplies the second column in A by any number (Here, I took 2)
 
 print A                        # Gives the desired output
